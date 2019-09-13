5a3fef16e326fa0eca0a904eed17eb31cf3c6161
mireille <mimimireille95@gmail.com>
2019-09-13 13:09:30 -0400

    nav b

--- app/request.py
+++ app/request.py
@@ -62,26 +62,26 @@ def process_sources(sources_list):
 
     return sources_results
 
-# def get_articles(id):
-#     '''
-#     function that gets the json response to url
-#     '''
-#     print(base_url_articles)
-#     print(id)
-#     print(api_key)
-#     get_articles_url = base_url_articles.format(id,api_key)
+def get_articles(id):
+    '''
+    function that gets the json response to url
+    '''
+    print(base_url_articles)
+    print(id)
+    print(api_key)
+    get_articles_url = base_url_articles.format(id,api_key)
 
-#     print(get_articles_url)
-#     with urllib.request.urlopen(get_articles_url) as url:
-#         get_articles_data = url.read()
-#         get_articles_response = json.loads(get_articles_data)
+    print(get_articles_url)
+    with urllib.request.urlopen(get_articles_url) as url:
+        get_articles_data = url.read()
+        get_articles_response = json.loads(get_articles_data)
         
-#         articles_results = None
-#         if get_articles_response['articles']:
-#             articles_results_list =  get_articles_response['articles']
-#             articles_results = process_article_results(articles_results_list)
+        articles_results = None
+        if get_articles_response['articles']:
+            articles_results_list =  get_articles_response['articles']
+            articles_results = process_article_results(articles_results_list)
 
-#     return articles_results
+    return articles_results
 
 
 def process_article_results(articles_list):
